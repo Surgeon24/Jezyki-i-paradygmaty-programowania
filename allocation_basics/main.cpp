6c5406c72935ffe202e030851d73c54cfbc2f49d
Surgeon24 <91936402+Surgeon24@users.noreply.github.com>
2021-10-18 19:29:26 +0200

    this is commit massage

--- allocation_basics/main.cpp
+++ allocation_basics/main.cpp
@@ -31,30 +31,63 @@ void tablica_2d(){
 
 //////////////////////////////////////////////////////////////////////////////////
 #include "math.h"
+#include <sstream>
+
+void manual(){
+    cout << "Simple calculatur\n"
+            "simpleCalc [nazwa dzialania]\n"
+            "\n"
+            "Dzialania:\n"
+            "add [a] [b]\n"
+            "    Dodawanie dwoch liczb ([a] i [b]) calkowitych.\n"
+            "subtract [a] [b]\n"
+            "    Odejmowanie od liczby [a] liczby [b].\n"
+            "volume [a] [b] [c] [h]"
+            "   obliczanie objetosci graniastoslupa prostego o podstawie trapezu\n"
+            "   [a] i [b] - podstawa trapezu, [c] - bok trapezu, [h] - wysokosc graniastoslupa\n";
+}
+
 int main (int argc, char *argv[]) {
     for(int i = 0; i < argc ; ++i) {
         cout << argv[i] <<endl;
     }
     string a = argv[1];
-    int x = (int)argv[2];
-    int y = (int)argv[3];
-    int z = (int)argv[4];
-    int h = (int)argv[5];
 
-    if (a == "add")
-        cout << "add: " << x + y;
-    else if (a == "subtract")
-        cout << "subtract: " << x - y;
+    if (a == "add") {
+        int my1, my2;
+        stringstream convert1(argv[2]);
+        convert1 >> my1;
+        stringstream convert2(argv[3]);
+        convert2 >> my2;
+        cout << "add: " << my1 << " + " << my2 << " = " << my1 + my2;
+    }
+    else if (a == "subtract") {
+        int my1, my2;
+        stringstream convert1(argv[2]);
+        convert1 >> my1;
+        stringstream convert2(argv[3]);
+        convert2 >> my2;
+        cout << "subtract: " << my1 - my2;
+    }
     else if (a == "volume"){
-        cout << "volume";
-        x = (x+y) * sqrt(z*z - (y-x)*(y-x)/4) / 2 * h;
-        cout << x;
+        cout << "volume: ";
+        int my1, my2, my3, my4;
+        stringstream convert1(argv[2]);
+        convert1 >> my1;
+        stringstream convert2(argv[3]);
+        convert2 >> my2;
+        stringstream convert3(argv[4]);
+        convert3 >> my3;
+        stringstream convert4(argv[5]);
+        convert4 >> my4;
+        cout << (my1+my2) * sqrt(my3*my3 - (my2-my1)*(my2-my1)/4) / 2 * my4;
     }
     else if (a == "help"){
-        cout << "help";
+        manual();
     }
     else {
-        cout << "wystapil blad";
+        cout << "wystapil blad!\n";
+        manual();
     }
 
     return 0;
