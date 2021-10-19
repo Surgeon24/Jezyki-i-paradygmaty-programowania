fdad37ff0a5cb23c0df0f5100ec2604f245a473a
Surgeon24 <91936402+Surgeon24@users.noreply.github.com>
2021-10-19 10:09:35 +0200

    commit

--- lab3_2/main.cpp
+++ lab3_2/main.cpp
@@ -18,6 +18,7 @@ void printarr (int *p_arr, int n){
     for (int i = 0; i < n; i++){
         cout << p_arr[i] << "  ";
     }
+    cout << endl;
 }
 
 int findmax (int *p_arr, int n){
@@ -29,14 +30,23 @@ int findmax (int *p_arr, int n){
     return max;
 }
 
+void reversearr (int *p_arr, int n){
+    for (int i = 0; i < n/2; i++){
+        swap(p_arr[i],p_arr[n-1-i]);
+    }
+}
+
 int main() {
     int a, b, x = 0;
-    cout << "wprowadz numer zadania: ";
+    cout << "Funkcja przyjmujaca tablice i dokonujaca na niej obliczen\n"
+            "wprowadz numer zadania: ";
     cin >> x;
+
     if (x == 1) {
         cin >> a >> b;
         cout << random(a, b);
     }
+
     else if (x == 2){
         int n;
         cout << "podaj rozmiar tablicy: ";
@@ -44,7 +54,19 @@ int main() {
         int *p_arr = new int[n];
         fillarr(p_arr, n);
         printarr(p_arr, n);
-        cout << "\nnajwieksza wartosc: " << findmax(p_arr, n);
+        cout << "najwieksza wartosc: " << findmax(p_arr, n);
+    }
+    else if (x == 3){
+        int n;
+        cout << "podaj rozmiar tablicy: ";
+        cin >> n;
+        int *p_arr = new int[n];
+        cout << "wypelnij tablice: \n";
+        for (int i = 0; i < n; i++){
+            cin >> p_arr[i];
+        }
+        reversearr (p_arr, n);
+        printarr(p_arr, n);
     }
     return 0;
 }
