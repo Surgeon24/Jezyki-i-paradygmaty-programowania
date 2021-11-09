a4c089a948f8b085b4f31b80b4881d871426912c
Surgeon24 <91936402+Surgeon24@users.noreply.github.com>
2021-11-09 09:31:28 +0200

    Commit

---
+++ lab_5_1/cmake-build-debug/CMakeFiles/lab_5_1.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lab_5_1.dir/src/Point.cpp.obj"
+  "CMakeFiles/lab_5_1.dir/src/main.cpp.obj"
+  "lab_5_1.exe"
+  "lab_5_1.exe.manifest"
+  "lab_5_1.pdb"
+  "liblab_5_1.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lab_5_1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
