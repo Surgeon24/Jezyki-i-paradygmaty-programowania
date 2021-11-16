234cfbbb8faa7be8f5bfcfd3bd0183ffdf2e254a
Surgeon24 <91936402+Surgeon24@users.noreply.github.com>
2021-11-16 09:22:35 +0200

    Commit

---
+++ lab6_4/src/vector.h
@@ -0,0 +1,18 @@
+
+
+#ifndef LAB6_4_VECTOR_H
+#define LAB6_4_VECTOR_H
+
+class Vector{
+private:
+    double x, y;
+
+public:
+    Vector();
+
+    Vector(double x, double y);
+
+    double length();
+};
+
+#endif //LAB6_4_VECTOR_H
